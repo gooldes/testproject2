0129a9233f6bdb707c02118c9a45f38c2f336f07
gooldes <gooldes@yandex.ru>
2019-01-21 22:59:51 +0300

    second change

--- v1/sections/header.php
+++ v1/sections/header.php
@@ -11,16 +11,19 @@
   <div class="container cf">
     <div class="flex rel">
       <div class="hdr__logo-cont flex">
-        <div class="hdr__logo"></div>
+        <div class="hdr__logo"><img src="v1/src/images/sp-common.png"></div>
         <div class="hdr__logo-line"></div>
         <div class="hdr__hsp"></div>
       </div>
-      <?php if($show_number) { ?>
+      <?php if(true/*$show_number*/) { ?>
         <div class="hdr__call">
+		<div class="phone_icon">icon</div>
+		<div class="phone_number">
           <a href="tel: <?php echo $ty ? $phone_ty : $phone; ?>">
             <p class="hdr__call-title">CALL FOR A FREE QUOTE</p>
             <p class="hdr__call-number"><?php echo $ty ? $phone_ty : $phone; ?></p>
           </a>
+		  </div>
         </div>
       <?php } ?>
     </div>
