43a1adc575bd846c8b0fd395389356f89a33041d
gooldes <gooldes@yandex.ru>
2019-01-22 14:19:14 +0300

    5 chages

--- v1/sections/body.php
+++ v1/sections/body.php
@@ -11,34 +11,34 @@
 	<span class="y-text">Competitive </span> Rates starting at 0.40%
 	</li>
 	<li>
-	<span class="y-text">C24/7</span> Dedicated customer
+	<span class="y-text">24/7</span> Dedicated customer <br>
 	support team
 	</li>
 	<li>
-	<span class="y-text">CEMV and PCI compliant</span>
+	<span class="y-text">EMV and PCI compliant</span><br>
 	plus NFC capable equipment
 	</li>
 	<li>
-	<span class="y-text">CFast & Easy</span> application process 
-	with no application fees!
+	<span class="y-text">Fast & Easy</span> application process 
+	with <span style="color: white; font-weight: 600;">no application fees!</span>
 	</li>
 	<ul>
 	</div>
 	<div class="table-footer">
-	<div class="left-block"><img src="v1/src/images/phone.png"> </div><div class="right-block">Call for a free quote <br> 888-888-8888</div>
+	<div class="left-block"><img src="v1/src/images/phone.png"> </div><div class="right-block"><p class="call-title">Call for a free quote</p> <p class="phone-number"> 888-888-8888</p></div>
 	</div>
 	</div> 
 	<div class="second-table">
-		<div class="table-header"><h2>START HERE</h2> <br> Get your FREE Personalized Quote </div>
+		<div class="table-header"><p class="start-text">START HERE</p><p class="start-under"> Get your FREE Personalized Quote</p></div>
 		<div class="table-body">
-			<p style="padding-top: 20px; padding-bottom: 20px; text-align:center; 	border-bottom: 2px solid #c8c8c8;">
+			<p class="under-header">
 			Are you currently processing
 			payments?
 			</p>
 			<div style="padding-top: 60px; padding-bottom: 60px;">
 			
-			<div class="y-button">YES >></div>
-			<div class="y-button">NO >></div>
+			<div class="y-button">YES »</div>
+			<div class="y-button">NO »</div>
 			</div>
 			<div class="pagination">
 			<div class="pagination-item active"></div>
@@ -61,10 +61,10 @@
 			</ul>
 		</div>
 		<div class="table-footer">
-		<strong>Answer a few questions </strong><br>
-		Learn More & Get a FREE Quote!
+		<p class="answer">Answer a few questions </p>
+		<p class="learn-more">Learn More & Get a FREE Quote!<p>
 		</div>
-		<small style="margin-left: 20px;"> *Free terminal only applies for approved Pura Payments merchant account and requires a 1 year contract. If you cancel your agreement  before the 1 year term has been completed a cancellation fee will apply. Does not include cost of shipping. </small>
+		<div class="small-text"> *Free terminal only applies for approved Pura Payments merchant account and requires a 1 year contract. If you cancel your agreement  before the 1 year term has been completed a cancellation fee will apply. Does not include cost of shipping. </div>
 	</div>
 	</div>
 	<div class="call-a-free"> 
